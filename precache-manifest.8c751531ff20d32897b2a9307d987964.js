f204e178333f67bc7b96e995d15c815156860c14
Meta Novitia <mnovitia@uci.edu>
2019-01-12 17:10:28 -0800

    Updates

---
+++ precache-manifest.8c751531ff20d32897b2a9307d987964.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d841d625e6ec9bb720e8e5f82bed2b88",
+    "url": "/sbhacks-v/static/media/home.d841d625.jpg"
+  },
+  {
+    "revision": "42722f9273124293eef4",
+    "url": "/sbhacks-v/static/js/runtime~main.42722f92.js"
+  },
+  {
+    "revision": "5b56987c3401dd66ad27",
+    "url": "/sbhacks-v/static/js/main.5b56987c.chunk.js"
+  },
+  {
+    "revision": "9545dd28316298e6ef7c",
+    "url": "/sbhacks-v/static/js/1.9545dd28.chunk.js"
+  },
+  {
+    "revision": "5b56987c3401dd66ad27",
+    "url": "/sbhacks-v/static/css/main.ef24f0ff.chunk.css"
+  },
+  {
+    "revision": "9545dd28316298e6ef7c",
+    "url": "/sbhacks-v/static/css/1.1257de69.chunk.css"
+  },
+  {
+    "revision": "5ea98b67454de4fa23dc8c87ff4fba27",
+    "url": "/sbhacks-v/index.html"
+  }
+];
